15b3069fe0ff59a062af4720d8f7b120c63d57be
MIRAVANI SRI S <107063870+miravanisri@users.noreply.github.com>
2025-09-16 12:54:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain_community.document_loaders import WebBaseLoader
 from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain.schema import Document
 from langchain.prompts import PromptTemplate
 from groq import Groq
@@ -51,8 +51,11 @@ if st.button("Run Analysis"):
 
             docs = [Document(page_content=c, metadata={"source":"resume"}) for c in resume_chunks] + \
                    [Document(page_content=d.page_content, metadata={"source":"job"}) for d in job_chunks]
-            vs = Chroma.from_documents(docs, embedding=embed,persist_directory=None)
+
+            texts = [doc.page_content for doc in docs]
             
+            vs = FAISS.from_texts(texts, embedding=embed)
+
             
             retriever = vs.as_retriever()
 
